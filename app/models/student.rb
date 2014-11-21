b6fca16f73631b7b263452ed88263095ffc57aa8
andrerpbts <andrerpbts@gmail.com>
2014-11-21 21:15:51 -0200

    Add status enumeration to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,13 @@
 class Student < ActiveRecord::Base
+  extend Enumerize
+
   validates :name, :register_number, presence: true
   validates :register_number, uniqueness: true
+
+  STATUSES = { enabled: 1, disabled: 2 }
+  enumerize :status,
+            in: STATUSES,
+            default: :enabled,
+            predicates: { prefix: true },
+            scope: true
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Student, :type => :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:register_number) }
   it { is_expected.to validate_uniqueness_of(:register_number) }
+  it { is_expected.to enumerize(:status)
+    .in(:enabled, :disabled).with_default(:enabled) }
 end
