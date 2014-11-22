6eabfc5e0c8043248936f5e67478fac6d3208db1
andrerpbts <andrerpbts@gmail.com>
2014-11-22 13:39:26 -0200

    Add home route to students

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'students#index'
+
   resources :courses, except: [:destroy]
   resources :students, except: [:destroy] do
     resources :classrooms, only: [:new, :create]
